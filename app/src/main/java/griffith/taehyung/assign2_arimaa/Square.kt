30133ac3d011e46679d1776c1256e4abf7966fbb
Taehyung <katalogin88@gmail.com>
2022-11-28 13:04:46 +0000

    create Square class

---
+++ app/src/main/java/griffith/taehyung/assign2_arimaa/Square.kt
@@ -0,0 +1,32 @@
+package griffith.taehyung.assign2_arimaa
+
+class Square {
+    // design trap tile (boolean)
+    var piece: Piece? = null
+    var isEmpty = true
+
+    // data to identify the type of pieces
+    val level: Int = piece!!.level
+    val color: Piece.PieceColor = piece!!.color
+
+    fun readSquare(): Char {
+        return if(isEmpty) {
+            ' '
+        } else piece!!.letter
+    }
+
+    // have new piece on square
+    fun havePiece(piece: Piece?) {
+        this.piece = piece
+        isEmpty = false
+    }
+
+    // remove piece on square
+    fun dropPiece(): Piece? {
+        val droppedPiece: Piece? = piece
+        piece = null
+        isEmpty = true
+
+        return droppedPiece
+    }
+}
--- app/src/main/java/griffith/taehyung/assign2_arimaa/Tile.kt
+++
@@ -1,5 +0,0 @@
-package griffith.taehyung.assign2_arimaa
-
-class Tile {
-
-}
